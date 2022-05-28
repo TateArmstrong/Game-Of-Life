317e908a11fe1c00d3d7c82c7d39a37c6da92b57
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-27 19:11:07 -0500

    Initial commit

---
+++ build.bat
@@ -0,0 +1,4 @@
+g++ -IC:\SFML-2.5.1\include -c main.cpp -o main.o
+g++ -LC:\SFML-2.5.1\lib .\main.o -o GameOfLife.exe -lsfml-graphics -lsfml-window -lsfml-system -mwindows
+del main.o
+GameOfLife.exe
