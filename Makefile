ac44c9ce9ae8db312aa99575fe999607477fc3fd
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-29 23:49:02 -0500

    Fixed printBoard function

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all: main.o
-
+	g++ -LC:\SFML-2.5.1\lib -static .\main.o -o GameOfLife.exe -lsfml-graphics -lsfml-window -lsfml-system
+	
 main.o: main.cpp
 	g++ -IC:\SFML-2.5.1\include -c main.cpp -o main.o
 
--- main.cpp
+++ main.cpp
@@ -3,21 +3,13 @@
 #include <windows.h>
 #include <time.h>
 
-const int CELL_SIZE = 8;
-const int WIDTH = 100;
-const int HEIGHT = 75;
+const float CELL_SIZE = 64.0f;
+const int WIDTH = 10;
+const int HEIGHT = 10;
 int currentBoard[WIDTH][HEIGHT];
 int nextBoard[WIDTH][HEIGHT];
 
-void UpdateCurrentBoard(){
-    for(int x = 0; x < WIDTH; x++){
-        for(int y = 0; y < HEIGHT; y++){
-            currentBoard[x][y] = nextBoard[x][y];
-        }
-    }
-}
-
-bool IsOutOfBounds(int row, int col){
+bool isOutOfBounds(int row, int col){
 	if(row < 0 || row >= WIDTH){
 		return true;
 	}
@@ -35,7 +27,7 @@ int getCellCount(int row, int col){
     int temp = 0;
 	for(int x = -1; x < 2; x++){
 		for(int y = -1; y < 2; y++){
-            if (IsOutOfBounds(row + x, col + y)){continue;}
+            if (isOutOfBounds(row + x, col + y)){continue;}
             if (x == 0 && y == 0){continue;}
             if (currentBoard[row + x][col + y] == 1){
                 temp++;
@@ -54,37 +46,59 @@ int getCellCount(int row, int col){
 	return temp;
 }
 
-void DrawCells(sf::RenderWindow &win, sf::RectangleShape &cell){
+void drawCells(sf::RenderWindow& win)
+{
+    sf::CircleShape cell(32.0f);
+
     for(int x = 0; x < WIDTH; x++){
-        for(int y = 0; y < HEIGHT; y++){
-            // Remember this is using nextBoard
-            if(currentBoard[x][y] == 1){
-                cell.setFillColor(sf::Color::White);
-                cell.setPosition(sf::Vector2f(x * CELL_SIZE, y * CELL_SIZE));
-                win.draw(cell);
-            }
-            else{
-                cell.setFillColor(sf::Color::Black);
-                cell.setPosition(sf::Vector2f(x * CELL_SIZE, y * CELL_SIZE));
-                win.draw(cell);
-            }
+    for(int y = 0; y < HEIGHT; y++){
+        if(currentBoard[x][y] == 1){
+            cell.setFillColor(sf::Color::Blue);
+            cell.setPosition(sf::Vector2f(x * CELL_SIZE, y * CELL_SIZE));
+            win.draw(cell);
         }
+        else{
+            cell.setFillColor(sf::Color::Green);
+            cell.setPosition(sf::Vector2f(x * CELL_SIZE, y * CELL_SIZE));
+            win.draw(cell);
+        }
+        // Updating the current board
+        currentBoard[x][y] = nextBoard[x][y];
+    }
     }
 }
 
-void updateCells(){
+void updateCells()
+{
     for(int x = 0; x < WIDTH; x++){
-        for(int y = 0; y < HEIGHT; y++){
+    for(int y = 0; y < HEIGHT; y++){
+        int cellCount = getCellCount(x, y);
+        switch(currentBoard[x][y]){
             // If the current cell is dead and it has 3 alive neighbors, it becomes alive
-            int cellCount = getCellCount(x, y);
-            switch(currentBoard[x][y]){
-                case 1:
-                    if(cellCount <= 1 || cellCount >= 4){nextBoard[x][y] == 0;}
-                case 0:
-                    if(cellCount == 3){nextBoard[x][y] = 1;}
-            }
+            case 0:
+                if(cellCount == 3){nextBoard[x][y] = 1;}
+            // If the current cell is alive 
+            case 1:
+                if(cellCount < 2 || cellCount > 3){nextBoard[x][y] = 0;}
         }
     }
+    }
+}
+
+void printBoard()
+{
+    for(int x = 0; x < WIDTH; x++){
+        for(int y = 0; y < HEIGHT; y++){
+            if(currentBoard[x][y] == 1){
+                std::cout << (char)178 << ' ';
+            }
+            else{
+                std::cout << (char)176 << ' ';
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "====================" <<std::endl;
 }
 
 int main()
@@ -93,7 +107,7 @@ int main()
 
     for(int x = 0; x < WIDTH; x++){
         for(int y = 0; y < HEIGHT; y++){
-            if(rand() % 100 + 1 < 10){
+            if(rand() % 100 + 1 < 20){
                 currentBoard[x][y] = 1;
             }
             else{
@@ -102,9 +116,9 @@ int main()
         }
     }
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Conway's Game of Life");
+    sf::RenderWindow window(sf::VideoMode(640, 640), "Conway's Game of Life");
 
-    sf::RectangleShape cell(sf::Vector2f(8.0f, 8.0f));
+    sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
     cell.setFillColor(sf::Color::White);
 
     // run the program as long as the window is open
@@ -119,20 +133,27 @@ int main()
                 window.close();
         }
         
-        updateCells();
+        //UpdateCurrentBoard();
+        //updateCells();
         //Sleep(2000);
+        printBoard();
+        std::cin.get();
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
         // draw everything here...
         // window.draw(...);
-        DrawCells(window, cell);
+        //DrawCells(window, cell);
+        drawCells(window);
 
         // end the current frame
         window.display();
-        UpdateCurrentBoard();
+        updateCells();
     }
+
+    printBoard();
+    std::cin.get();
 
     return 0;
 }
